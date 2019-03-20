6ec0ef7513019cc34b0c5180db1373114099f845
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-20 16:53:10 -0400

    added props to Animal model

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -1,7 +1,10 @@
 namespace safari_api.Models
 {
-    public class Animal
-    {
-        
-    }
+  public class Animal
+  {
+    public int Id { get; set; }
+    public string Species { get; set; }
+    public int CountOfTimesSeen { get; set; }
+    public string LocationOfLastSeen { get; set; }
+  }
 }
