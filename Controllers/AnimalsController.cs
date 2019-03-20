fd2f7bffe5f978262c5381cc47d26213f8c046d9
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-20 17:00:34 -0400

    changed api call returns to animals

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using safari_api.Models;
 
 namespace safari_api.Controllers
 {
@@ -12,21 +13,21 @@ namespace safari_api.Controllers
   {
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public ActionResult<IEnumerable<Animal>> Get()
     {
       return new string[] { "value1", "value2" };
     }
 
     // GET api/values/5
     [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
+    public ActionResult<Animal> Get(int id)
     {
       return "value";
     }
 
     // POST api/values
     [HttpPost]
-    public void Post([FromBody] string value)
+    public void Post([FromBody] string species)
     {
     }
   }
