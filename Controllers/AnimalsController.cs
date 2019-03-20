9fd51cd370c6812c96598e520903ebdea3e8477d
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-20 16:47:07 -0400

    deleted post and delete

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -29,17 +29,5 @@ namespace safari_api.Controllers
     public void Post([FromBody] string value)
     {
     }
-
-    // PUT api/values/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/values/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
   }
 }
