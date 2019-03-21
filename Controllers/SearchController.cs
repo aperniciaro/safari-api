3440eb1807de218307b36dd5c1d86b2d8851479f
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-21 10:50:45 -0400

    added search endpoint

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using safari_api.Models;
+
+namespace safari_api.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SearchController
+  {
+    [HttpGet]
+    public ActionResult<string> SearchAnimal([FromQuery] string query)
+    {
+      return query;
+    }
+  }
+}
